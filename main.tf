74e689e78983bb462f172dd8c41ada68dde6bfba
rokar1908 <84006029+rokar1908@users.noreply.github.com>
2021-06-14 14:52:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_subnet" "sn" {
    enforce_private_link_endpoint_network_policies = true
 }
 resource "azurerm_subnet" "sn1" {
-  name                 = "VM"
+  name                 = "privatelink"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.2.0/24"]
